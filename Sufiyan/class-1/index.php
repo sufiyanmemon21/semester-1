23e0a5a62f5fee9e02eea7d5a74a9bdd61caa704
sufiyanmemon21 <sufiyanmemon393@gmail.com>
2025-07-26 12:59:36 +0500

    Add files via upload

---
+++ Sufiyan/class-1/index.php
@@ -0,0 +1,17 @@
+.0<?php
+
+// echo "hello echo method <br>";
+// print "hello print method"
+
+//variables//
+
+$name = "ali";
+$email = "ali@gmail.com";
+$name = ["CPISM","DISM","HDSE"];
+
+
+echo "<pre>";
+var_dump($name);
+echo "</pre>"
+
+?>
